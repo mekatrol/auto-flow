04d0e0ecab203128043e192a77a6150a3fa79850
PW <mekatrol@wojcik.com.au>
2024-07-14 20:56:54 +1000

    Remove scroll bar triangles

--- editor/src/flow/components/SvgScrollbar.vue
+++ editor/src/flow/components/SvgScrollbar.vue
@@ -16,31 +16,13 @@
     <rect
       style="pointer-events: none"
       :x="x"
-      :y="triangleHeight + scroll * sliderHeight"
+      :y="scroll * sliderHeight"
       :width="width"
       :height="Math.max(0, sliderHeight)"
       :fill="sliderDragging ? '#bbb' : '#fff'"
       stroke="black"
       stroke-width="1px"
     ></rect>
-    <!-- The decrement value triangle -->
-    <path
-      :transform="`translate(${x}, ${y})`"
-      :d="`M ${width / 2} 0 l ${width / 2} ${triangleHeight} l ${-width} 0 z`"
-      fill="white"
-      stroke="black"
-      stroke-width="1px"
-      @pointerdown="(e) => emit('scrollUp', e)"
-    ></path>
-    <!-- The increment value triangle -->
-    <path
-      :transform="`translate(${x}, ${y})`"
-      :d="`M 0 ${height - triangleHeight} l ${width} 0 l ${-width / 2} ${triangleHeight} z`"
-      fill="white"
-      stroke="black"
-      stroke-width="1px"
-      @pointerdown="(e) => emit('scrollDown', e)"
-    ></path>
   </g>
 </template>
 
@@ -63,10 +45,8 @@ const emit = defineEmits(['scrollUp', 'scrollDown', 'scroll']);
 
 const scrollbar = ref();
 
-const triangleHeight = ref(15);
-
 // How much of the height is the scroll height
-const scrollHeight = computed(() => props.height - 2 * triangleHeight.value);
+const scrollHeight = computed(() => props.height);
 
 // How large a step size if for (max - min) items
 const sliderHeight = computed(() => scrollHeight.value / props.count);
@@ -88,7 +68,7 @@ const dragSliderEnd = (e: PointerEvent) => {
 
 const dragSliderMove = (e: PointerEvent) => {
   if (sliderDragging.value) {
-    const scrollValue = Math.min(props.count - 1, Math.max(0, Math.floor((e.offsetY - triangleHeight.value) / sliderHeight.value)));
+    const scrollValue = Math.min(props.count - 1, Math.max(0, Math.floor(e.offsetY / sliderHeight.value)));
     emit('scroll', scrollValue);
   }
 };
