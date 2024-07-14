a37b5ccbc22e1c7349d38fa9463eee7310044584
PW <mekatrol@wojcik.com.au>
2024-07-15 08:48:35 +1000

    Split flow part into FlowControl

--- editor/src/flow/components/EditorControl.vue
+++ editor/src/flow/components/EditorControl.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="flow-designer"
+    class="flow-editor"
     :viewBox="`0 0 ${svgWidth} ${svgHeight}`"
     ref="svg"
     @pointermove="(e) => flowDesigner.pointerMove(e)"
@@ -38,38 +38,16 @@
           :height="svgHeight"
           :grid-size="gridSize"
         />
-
-        <ConnectionControl
-          v-for="(connection, i) in flowDesigner.connections.value"
-          :key="i"
-          :connection="connection"
-        />
-        <BlockControl
-          v-for="(block, i) in flowDesigner.blocks.value"
-          :key="i"
-          :block="block"
-        />
-        <ConnectingControl
-          v-if="flowDesigner.drawingConnection.value"
-          :connecting="flowDesigner.drawingConnection.value!"
-        />
-        <BlockControl
-          v-if="flowDesigner.dragBlock.value && flowDesigner.dragBlock.value.draggingAsNew"
-          :block="flowDesigner.dragBlock.value"
-        />
       </ContainerControl>
     </g>
   </svg>
 </template>
 
 <script setup lang="ts">
-import ConnectionControl from './ConnectionControl.vue';
-import ConnectingControl from './ConnectingControl.vue';
 import BlockPalletteControl from './BlockPalletteControl.vue';
 import FlowControl from './FlowControl.vue';
 import { onMounted, ref, watch } from 'vue';
 import { useScreenSize } from 'vue-boosted';
-import BlockControl from './BlockControl.vue';
 import { initFlowDesigner } from '../types/FlowDesigner';
 import { useMockStore } from '../stores/mock-store';
 import ContainerControl from './ContainerControl.vue';
@@ -120,21 +98,3 @@ watch(
   }
 );
 </script>
-
-<style scoped lang="scss">
-.flow-designer {
-  width: 100%;
-  height: 100%;
-  transform-origin: top left;
-
-  .editor {
-    border: 1px solid #b6b3b6;
-  }
-
-  .grid-line {
-    stroke: #aaaaaa77;
-    stroke-width: 0.3;
-    pointer-events: none;
-  }
-}
-</style>
--- editor/src/flow/components/FlowControl.vue
+++ editor/src/flow/components/FlowControl.vue
@@ -4,10 +4,35 @@
     :height="height"
     :grid-size="gridSize"
   />
+
+  <ConnectionControl
+    v-for="(connection, i) in flowDesigner.connections.value"
+    :key="i"
+    :connection="connection"
+  />
+  <BlockControl
+    v-for="(block, i) in flowDesigner.blocks.value"
+    :key="i"
+    :block="block"
+  />
+  <ConnectingControl
+    v-if="flowDesigner.drawingConnection.value"
+    :connecting="flowDesigner.drawingConnection.value!"
+  />
+  <BlockControl
+    v-if="flowDesigner.dragBlock.value && flowDesigner.dragBlock.value.draggingAsNew"
+    :block="flowDesigner.dragBlock.value"
+  />
 </template>
 
 <script setup lang="ts">
 import GridControl from './GridControl.vue';
+import ConnectionControl from './ConnectionControl.vue';
+import ConnectingControl from './ConnectingControl.vue';
+import BlockControl from './BlockControl.vue';
+import { useFlowDesigner } from '../types/FlowDesigner';
+
+const flowDesigner = useFlowDesigner();
 
 interface Props {
   width: number;
