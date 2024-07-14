e135f9d0d4915da896724b5362370729de32a583
PW <mekatrol@wojcik.com.au>
2024-07-14 21:42:47 +1000

    Mouse wheel on scrollbar

--- editor/src/flow/components/SvgScrollbar.vue
+++ editor/src/flow/components/SvgScrollbar.vue
@@ -11,6 +11,7 @@
       @pointerdown="dragSliderStart"
       @pointerup="dragSliderEnd"
       @pointermove="dragSliderMove"
+      @mousewheel="mouseWheel"
     ></rect>
     <!-- The slider -->
     <rect
@@ -28,6 +29,7 @@
 
 <script setup lang="ts">
 import { computed, ref } from 'vue';
+import type { Offset } from '../types/Offset';
 
 interface Props {
   x: number;
@@ -45,6 +47,7 @@ const emit = defineEmits(['scrollUp', 'scrollDown', 'scroll']);
 
 const scrollbar = ref();
 const sliderPointerEvent = ref<PointerEvent | undefined>(undefined);
+const sliderPointDownOffset = ref<Offset>({ x: 0, y: 0 });
 
 // How much of the height is the scroll height
 const scrollHeight = computed(() => props.height);
@@ -55,21 +58,43 @@ const sliderHeight = computed(() => scrollHeight.value / props.count);
 const sliderDragging = ref(false);
 
 const dragSliderStart = (e: PointerEvent) => {
-  sliderDragging.value = true;
-  sliderPointerEvent.value = e;
+  // Button zero is normally the main button (eg left mouse button)
+  if (e.button !== 0) {
+    return;
+  }
 
+  // We are now dragging the slider
+  sliderDragging.value = true;
+
+  // We the mouse button is clicked then we need to calculate what the scroll value would be at that location
+  const offsetAdjust = Math.min(props.count - 1, Math.max(0, Math.floor(e.offsetY / sliderHeight.value)));
+
+  // We need to track the offset of the mouse from the top of the slider at the calculated scroll position
+  // so that the slider doesn't 'jump'
+  sliderPointDownOffset.value = { x: e.offsetX, y: e.offsetY - offsetAdjust * sliderHeight.value };
+
+  // Capture pointer events so that we can track the mouse outside of the scroll bar if the user has not released the mouse button
   (scrollbar.value as SVGTextElement).setPointerCapture(e.pointerId);
 
+  // Call move event to emit scroll value
   dragSliderMove(e);
 };
 
 const dragSliderEnd = (e: PointerEvent) => {
+  // Button zero is normally the main button (eg left mouse button)
+  if (e.button !== 0) {
+    return;
+  }
+
+  // Release pointer events
   (scrollbar.value as SVGTextElement).releasePointerCapture(e.pointerId);
+
   sliderDragging.value = false;
   sliderPointerEvent.value = undefined;
 };
 
 const dragSliderMove = (e: PointerEvent) => {
+  // Only process event if currently dragging slider
   if (sliderDragging.value) {
     sliderPointerEvent.value = e;
     const scrollValue = Math.min(props.count - 1, Math.max(0, Math.floor(e.offsetY / sliderHeight.value)));
@@ -77,10 +102,15 @@ const dragSliderMove = (e: PointerEvent) => {
   }
 };
 
+const mouseWheel = (e: WheelEvent) => {
+  const delta = e.deltaY / 100;
+  emit('scroll', Math.min(props.count - 1, Math.max(0, Math.floor(props.scroll + delta))));
+};
+
 const sliderY = computed(() => {
   return sliderDragging.value && sliderPointerEvent.value
     ? // relative to mouse position (to make slide smooth)
-      Math.min(props.height - sliderHeight.value, Math.max(0, sliderPointerEvent.value.offsetY - sliderHeight.value / 2))
+      Math.min(props.height - sliderHeight.value, Math.max(0, sliderPointerEvent.value.offsetY - sliderPointDownOffset.value.y))
     : // based on scroll value
       props.scroll * sliderHeight.value;
 });
